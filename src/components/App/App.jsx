91559bda26a7c93120be869979ec9f5e23439b22
Serhii01010101 <jobjob2109@gmail.com>
2024-10-14 21:21:14 +0300

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,11 +3,19 @@ import ContactList from "../ContactList/ContactList";
 import SearchForm from "../SearchBox/SearchBox";
 import userData from "../../Contacts.json";
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [contacts, setContacts] = useState(userData);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem("contacts");
+    return savedContacts ? JSON.parse(savedContacts) : userData;
+  });
+
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   const addContact = (newContact) => {
     setContacts((prevContacts) => {
@@ -22,7 +30,7 @@ function App() {
   };
 
   const visibleContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLocaleLowerCase())
+    contact.name.toLowerCase().includes(filter.toLowerCase())
   );
 
   return (
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,29 +1,58 @@
 import css from "./ContactForm.module.css";
+
 import { nanoid } from "nanoid";
+import * as Yup from "yup";
+
+import { Formik, Form, Field } from "formik";
+
+import { ErrorMessage } from "formik";
 
 const ContactForm = ({ onAdd }) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (values, { resetForm }) => {
     onAdd({
       id: nanoid(),
-      name: e.target.elements.name.value,
-      number: e.target.elements.number.value,
+      name: values.name,
+      number: values.number,
     });
-    e.target.reset();
+    resetForm();
   };
-  return (
-    <form className={css.form} onSubmit={handleSubmit}>
-      <label htmlFor="" className={css.label}>
-        <span>Name</span>
-        <input type="text" name="name" />
-      </label>
+  const initialValues = {
+    name: "",
+    number: "",
+  };
 
-      <label htmlFor="" className={css.label}>
-        <span>Number</span>
-        <input type="text" name="number" />
-      </label>
-      <button type="submit"> Add contact</button>
-    </form>
+  const FeedbackSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    number: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+  });
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={FeedbackSchema}
+    >
+      <Form className={css.form}>
+        <div>
+          <Field type="text" name="name" className={css.field} />
+          <ErrorMessage name="name" component="span" className={css.error} />
+        </div>
+        <div>
+          <Field type="text" name="number" className={css.field} />
+          <ErrorMessage name="number" component="span" className={css.error} />
+        </div>
+
+        <button type="submit" className={css.btn}>
+          Add contact
+        </button>
+      </Form>
+    </Formik>
   );
 };
 
